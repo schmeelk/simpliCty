1f1c1dad1ae4fd892077c20e8bdc3b3710c61aee
Suzanna Schmeelk <ss4648@columbia.edu>
2016-07-25 17:51:57 +0000

    Regression testing

---
+++ src/regression-tests/test.sh
@@ -0,0 +1,190 @@
+#!/bin/sh
+# Project:  COMS S4115, SimpliCty Compiler
+# Filename: src/testall.sh
+# Authors:  - Rui Gu,           rg2970
+#           - Adam Hadar,       anh2130
+#           - Zachary Moffitt,  znm2104
+#           - Suzanna Schmeelk, ss4648
+# Purpose:  * Regression testing script for SimpliCty
+#           * Steps through list of files:
+#             * Expected to work: compile, run, check output
+#             * Expected to fail: compile, check error
+# Modified: 2016-07-24
+
+# Path to the LLVM interpreter
+LLI="lli"
+#LLI="/usr/local/opt/llvm/bin/lli"
+
+# Path to the simpliCty compiler.  Usually "./simplicty.native"
+# Try "_build/simplicty.native" if ocamlbuild was unable to create a symbolic link.
+SIMPLICTY="../simplicty.native"
+
+# Set time limit for all operations
+ulimit -t 30
+
+globallog=test.log
+rm -f $globallog
+error=0
+globalerror=0
+dirout=./test-output/
+keep=0
+
+Usage() {
+    echo "Usage: testall.sh [options] [.mc files]"
+    echo "-k    Keep intermediate files"
+    echo "-h    Print this help"
+    exit 1
+}
+
+SignalError() {
+    if [ $error -eq 0 ] ; then
+	echo "FAILED"
+	error=1
+    fi
+    echo "  $1"
+}
+
+# Compare <outfile> <reffile> <difffile>
+# Compares the outfile with reffile.  Differences, if any, written to difffile
+Compare() {
+    generatedfiles="$generatedfiles $3"
+    echo diff -b ${dirout}$1 $2 ">" $3 1>&2
+    diff -b "${dirout}$1" "$2" > "${dirout}$3" 2>&1 || {
+	SignalError "$1 differs"
+	echo "FAILED $1 differs from $2" 1>&2
+    }
+}
+
+# Run <args>
+# Report the command, run it, and report any errors
+Run() {
+    echo $* 1>&2
+    eval $* || {
+	SignalError "$1 failed on $*"
+	return 1
+    }
+}
+
+# RunFail <args>
+# Report the command, run it, and expect an error
+RunFail() {
+    echo $* 1>&2
+    eval $* && {
+	SignalError "failed: $* did not report an error"
+	return 1
+    }
+    return 0
+}
+
+Check() {
+    error=0
+    basename=`echo $1 | sed 's/.*\\///
+                             s/.sct//'`
+    reffile=`echo $1 | sed 's/.sct$//'`
+    basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
+
+    echo -n "$basename..."
+
+    echo 1>&2
+    echo "###### Testing $basename" 1>&2
+
+    generatedfiles=""
+
+    generatedfiles="$generatedfiles ${basename}.ll ${basename}.out" &&
+    Run "$SIMPLICTY" "<" $1 ">" "${dirout}${basename}.ll" &&
+    Run "$LLI" "${dirout}${basename}.ll" ">" "${dirout}${basename}.out" &&
+    Compare ${basename}.out ${reffile}.out ${basename}.diff
+
+    # Report the status and clean up the generated files
+
+    if [ $error -eq 0 ] ; then
+	if [ $keep -eq 0 ] ; then
+	    rm -f $generatedfiles
+	    rm -f ${dirout}/* 
+	fi
+	echo "OK"
+	echo "###### SUCCESS" 1>&2
+    else
+	echo "###### FAILED" 1>&2
+	globalerror=$error
+    fi
+}
+
+CheckFail() {
+    error=0
+    basename=`echo $1 | sed 's/.*\\///
+                             s/.sct//'`
+    reffile=`echo $1 | sed 's/.sct$//'`
+    basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
+
+    echo -n "$basename..."
+
+    echo 1>&2
+    echo "###### Testing $basename" 1>&2
+
+    generatedfiles=""
+
+    generatedfiles="$generatedfiles ${basename}.err ${basename}.diff" &&
+    RunFail "$SIMPLICTY" "<" $1 "2>" "${dirout}${basename}.err" ">>" $globallog &&
+    Compare ${basename}.err ${reffile}.err ${basename}.diff
+
+    # Report the status and clean up the generated files
+
+    if [ $error -eq 0 ] ; then
+	if [ $keep -eq 0 ] ; then
+	    rm -f $generatedfiles
+	fi
+	echo "OK"
+	echo "###### SUCCESS" 1>&2
+    else
+	echo "###### FAILED" 1>&2
+	globalerror=$error
+    fi
+}
+
+while getopts kdpsh c; do
+    case $c in
+	k) # Keep intermediate files
+	    keep=1
+	    ;;
+	h) # Help
+	    Usage
+	    ;;
+    esac
+done
+
+shift `expr $OPTIND - 1`
+
+LLIFail() {
+  echo "Could not find the LLVM interpreter \"$LLI\"."
+  echo "Check your LLVM installation and/or modify the LLI variable in testall.sh"
+  exit 1
+}
+
+which "$LLI" >> $globallog || LLIFail
+
+
+if [ $# -ge 1 ]
+then
+    files=$@
+else
+    files="tests/test/test-*.sct tests/fail/fail-*.sct"
+fi
+
+for file in $files
+do
+    case $file in
+	*test-*)
+	    Check $file 2>> $globallog
+	    ;;
+	*fail-*)
+	    CheckFail $file 2>> $globallog
+	    ;;
+	*)
+	    echo "unknown file type $file"
+	    globalerror=1
+	    ;;
+    esac
+done
+
+exit $globalerror
